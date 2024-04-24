38eb4a8574d6f4f1ca9e2341e89f80b4363bcac8
jsdydaktyk <jsdydaktyka@gmail.com>
2024-04-24 15:12:19 +0200

    Add Second Fragment

--- app/src/main/java/com/example/tests/FirstFragment.kt
+++ app/src/main/java/com/example/tests/FirstFragment.kt
@@ -1,10 +1,12 @@
 package com.example.tests
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,7 +36,19 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
+
         return inflater.inflate(R.layout.fragment_first, container, false)
+
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val myImage = view.findViewById<ImageView>(R.id.imageView_maja)
+        myImage.setOnClickListener{
+            Log.i("maja", "klik")
+        }
     }
 
     companion object {
