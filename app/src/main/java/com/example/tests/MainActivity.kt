c068c62ebef7bed14cf1e83d7aed6736fc2be3cf
jsdydaktyk <jsdydaktyka@gmail.com>
2024-04-24 14:08:39 +0200

    Initial commit

---
+++ app/src/main/java/com/example/tests/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.tests
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val myList =findViewById<ListView>(R.id.mojalista)
+        val mojeCele = resources.getStringArray(R.array.mojecele)
+        var arrayAdapter = ArrayAdapter(this,
+            android.R.layout.simple_list_item_1,
+            mojeCele
+            )
+        myList.adapter=arrayAdapter
+    }
+}
